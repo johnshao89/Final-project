214fe719412471160a217c5be3317f9832da9085
Wenjie Shao <wjshao2010@gmail.com>
2019-04-07 21:47:07 -0400

    update

--- Final Project/app.py
+++ Final Project/app.py
@@ -23,10 +23,37 @@ def input_data():
     in_rate=request.form["rate"]
     in_time=request.form["reaction_time"]
     in_solvent=request.form["solvents"]
-    store_data(in_project,in_temperature,in_rate,in_time,in_solvent)
+    with open("project_data.txt","a") as storage:
+            print(in_project,in_temperature,in_rate,in_time,in_solvent, sep="|",file=storage)
     return render_template("results.html", the_project=in_project,
     the_temperature=in_temperature,the_rate=in_rate,the_time=in_time,
     the_solvent=in_solvent)
+
+#define the website to view the overall result
+@app.route("/sum_info")
+def sum_data():
+        content=[]
+        titles=["Project Code","Temperature","Agitation Rate","Reaction Time","Solvent"]
+        with open ("project_data.txt") as exp_info:
+                for line in exp_info:
+                        content.append([])
+                        for item in line.strip().split("|"):
+                                content[-1].append(item)
+        return render_template("sum_info.html", row_titles=titles,the_data=content)
+
+#define the website for specific project results
+@app.route("/project",methods=["POST"])
+def sel_project():
+        project_code=request.form["sum_projects"]
+        content=[]
+        titles=["Project Code","Temperature","Agitation Rate","Reaction Time","Solvent"]
+        with open ("project_data.txt") as exp_info:
+                for line in exp_info:
+                        if line.strip().split("|")[0]==project_code:
+                                content.append([])
+                                for item in line.strip().split("|"):
+                                        content[-1].append(item)
+        return render_template("single_project.html", row_titles=titles,the_data=content)
 
 
 
